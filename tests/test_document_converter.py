5654ae90cfdd531f99503295174fa486c9607a2e
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-01-27 11:22:29 +0530

    Making file organisation reasonable.

--- tests/test_document_converter.py
+++ tests/test_document_converter.py
@@ -1,3 +1,9 @@
+import sys
+sys.path.append('..')
+sys.path.append('../converters')
+sys.path.append('test_files')
+
+
 import unittest
 
 from document_converter import convert
@@ -7,10 +13,10 @@ class ConvertTests(unittest.TestCase):
         #Note: test4.html contains javascript so this test also demonstrates
         #stripping of <script> elements.
         self.failUnless(
-            convert(['test1.html', 'test2.html', 'test3.html'],
+            convert(['test1.html', 'test2.html', 'test3.html', 'test4.html'],
                     ['pdf', 'txt']) ==
-            [['test1.pdf', 'test2.pdf', 'test3.pdf'], 
-             ['test1.txt', 'test2.txt', 'test3.txt']])
+            [['test1.pdf', 'test2.pdf', 'test3.pdf', 'test4.pdf'], 
+             ['test1.txt', 'test2.txt', 'test3.txt', 'test4.txt']])
 
 
 def main():
