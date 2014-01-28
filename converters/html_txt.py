ffc9e0ea6967645e3c4b7d3c269291dda8577aeb
Saurabh Jha <saurabh.jhaa@gmail.com>
2014-01-28 18:27:42 +0530

    Fixing more bugs

--- converters/html_txt.py
+++ converters/html_txt.py
@@ -1,10 +1,11 @@
 import sys
-sys.path.insert(0, '..')
+sys.path.append('..')
 
 from general import GeneralConverter
 from html2text import html2text
 from file_manager import FileManager
 from bs4 import BeautifulSoup
+import re
 
 class HtmlTxt(GeneralConverter):
     """
@@ -22,8 +23,13 @@ class HtmlTxt(GeneralConverter):
             bytestream = input_file_object.get_stream()
             soup = BeautifulSoup(bytestream)
             [s.extract() for s in soup('script')]
+            [s.extract() for s in soup('style')]
             bytestream = unicode(soup)
             outputstream = html2text(bytestream)
+            outputstring = str(outputstream)
+            expression = re.compile('!\[\]\(data:image.*\)')
+            outputstring = re.sub(expression, '', outputstring)
+            outputstream = unicode(outputstring)
             output_file = input_file_object.write(output_extension, outputstream)
             if output_file:
                 return FileManager(output_file)
